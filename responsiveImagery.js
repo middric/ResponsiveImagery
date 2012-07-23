8c510415ab125207e0070302cb5a69aaa487c1ec
Rich Middleditch <richmiddleditch@gmail.com>
2012-07-23 10:27:19 +0100

    Linting

--- responsiveImagery.js
+++ responsiveImagery.js
@@ -1,19 +1,19 @@
-/* 
+/*
 					ReponsiveImagery.js v0.3
 
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-                    Version 2, December 2004 
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
 
  Copyright (C) 2012 Rich Middleditch <@middric>
 
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this license document, and changing it is allowed as long 
- as the name is changed. 
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
 
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
+  0. You just DO WHAT THE FUCK YOU WANT TO.
 */
 
 (function ($) {
@@ -54,7 +54,7 @@
 				matches = src.match(pattern.regex);
 				for (i = 0, j = 0; i < variables.length; i++, j++) {
 					matches[pattern.variables[i]] = variables[j];
-				};
+				}
 
 				matches.shift();
 				url = matches.join('');
